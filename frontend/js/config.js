1fb07e0fd1511c9baa348b8006ad6a404f2414f5
Hameem-ahmad <f2024408155@umt.edu.pk>
2026-06-17 16:52:04 +0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,3 @@
-
 const API_BASE_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
   ? "http://127.0.0.1:8000"
-  : "https://student-management-api-production-a951.up.railway.app/";
+  : "https://student-management-api-production-a951.up.railway.app";
